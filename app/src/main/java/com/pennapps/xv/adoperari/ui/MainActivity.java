3985c4eea7c403dd3be2442969f4eb693b44ca47
dp1851 <Dev@Dineshs-MacBook-Pro.local>
2017-01-22 03:20:47 -0500

    Hellol
    zHi

--- app/src/main/java/com/pennapps/xv/adoperari/ui/MainActivity.java
+++ app/src/main/java/com/pennapps/xv/adoperari/ui/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.EditText;
